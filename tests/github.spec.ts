06696468564a1ab9fd13db6b98ca94cbce4997a6
Praveen Roy <pkroy0505@gmail.com>
2026-01-09 19:10:59 +0530

    added github

---
+++ tests/github.spec.ts
@@ -0,0 +1,25 @@
+// Github/Bitbucket/Gitlab - Version Control Tool
+
+// To work with github we need to have git installed in our system
+// We can download the git tool from https://git-scm.com/downloads
+
+// If the repository is already available:
+// 1. Clone the respository in your local system - git clone <repo_url>
+// 2. Install all the dependencies - "npm install" - "npm playwright install"
+
+// 3. Create a new branch - TS01LoginPage - "git checkout -b TS01LoginPage"
+// 4. Validate if the new branch has been created successfully - git branch
+// 5. Make the changes in the code and save the file
+// 6. The files which have been modified or added - "git status"
+// 7. Stage all the modified files - "git add ."
+// 8. To commit the changes inside the local git - "git commit -m "added tag" "
+     // 8.1 - Switch to the main branch - master/main/release and make a pull of the recent code - "git checkout master"
+     // 8.2 - To pull all the recent changes from the master branch - git pull
+     // 8.3 - Switch back to your feature branch - "git checkout TS01LoginPage"
+     // 8.4 - Merge the master branch changes to your feature branch - "git merge master"
+// 9. Push the changes to remote repository - "git push origin TS01LoginPage"
+// 10. Go to the github repository and create a pull request for your branch
+
+
+// 3.0 - Pull the latest code from the remote repository to the master branch inside your local system.
+// #3-#10
